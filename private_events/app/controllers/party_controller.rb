44edb9c5331114cdb5cf6046a518826d7ecbc2a8
Andres Munoz <andrefe91@gmail.com>
2024-02-07 23:43:13 -0500

    Modification to the views and Controllers/Models

--- private_events/app/controllers/party_controller.rb
+++ private_events/app/controllers/party_controller.rb
@@ -1,5 +1,28 @@
 class PartyController < ApplicationController
+  before_action :authenticate_user!
   def index
-    
+    @parties = current_user.parties
+  end
+
+  def new
+    @party = current_user.parties.build()
+  end
+
+  def create
+    @party = current_user.parties.build(party_params)
+
+    if @party.save
+      redirect_to party_path(@party)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+
+  def party_params
+    params.require(:party).permit(:place, :date)
   end
 end
