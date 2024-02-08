78000cdeb3621691288b456eb69ac6c98647e29d
Joshua <andrefe91@gmail.com>
2024-02-08 14:58:09 -0500

    Working association

--- private_events/app/models/party.rb
+++ private_events/app/models/party.rb
@@ -1,5 +1,5 @@
 class Party < ApplicationRecord
-  has_and_belongs_to_many :user
+  has_and_belongs_to_many :users
   # has_and_belongs_to_many :creator, class_name: 'User', foreign_key: 'user_id'
   validates :place, :date, presence: true
 end
