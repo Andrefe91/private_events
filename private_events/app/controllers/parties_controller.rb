9964b7a59d8b4f6a99b391a889050fff3d00a201
Andres Munoz <andrefe91@gmail.com>
2024-02-08 20:41:31 -0500

    Named the controller wrong, starting again.

---
+++ private_events/app/controllers/parties_controller.rb
@@ -0,0 +1,26 @@
+class PartiesController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @parties = current_user.parties
+  end
+
+  def new
+    @parties = current_user.parties.build()
+  end
+
+  def create
+    @parties = current_user.parties.create(party_params)
+
+    if @parties.save
+      redirect_to parties_path(@parties)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def party_params
+    params.require(:parties).permit(:place, :date)
+  end
+end
--- private_events/test/controllers/parties_controller_test.rb
+++ private_events/test/controllers/parties_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class PartyControllerTest < ActionDispatch::IntegrationTest
+class PartiesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get party_index_url
     assert_response :success
