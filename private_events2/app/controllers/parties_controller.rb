fbfb115ba46f866553fdcb5daafacc52f0e71115
Andres Munoz <andrefe91@gmail.com>
2024-02-08 21:57:45 -0500

    Finishes the has_and_belongs_to_many test

--- private_events2/app/controllers/parties_controller.rb
+++ private_events2/app/controllers/parties_controller.rb
@@ -1,18 +1,19 @@
 class PartiesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @parties = current_user.parties
+    @party = current_user.parties
   end
 
   def new
-    @parties = current_user.parties.build()
+    @party = current_user.parties.build()
   end
 
   def create
-    @parties = current_user.parties.create(party_params)
+    @party = current_user.parties.build(party_params)
+    current_user.parties << @party
 
-    if @parties.save
-      redirect_to parties_path(@parties)
+    if @party.save
+      redirect_to parties_path(@party)
     else
       render :new, status: :unprocessable_entity
     end
--- private_events2/app/models/party.rb
+++ private_events2/app/models/party.rb
@@ -1,3 +1,5 @@
 class Party < ApplicationRecord
   has_and_belongs_to_many :users
+
+  validates :place, :date, presence: true
 end
