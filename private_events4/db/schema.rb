5390a7eb9a1a3d6e5a7920d802ce9954d9998fc0
Andres Munoz <andrefe91@gmail.com>
2024-02-11 23:04:40 -0500

    Invitation model and Associations

--- private_events4/db/schema.rb
+++ private_events4/db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_11_215124) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_11_222212) do
   create_table "events", force: :cascade do |t|
     t.string "place"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_invitations_on_event_id"
+    t.index ["user_id"], name: "index_invitations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_11_215124) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
+  add_foreign_key "invitations", "events"
+  add_foreign_key "invitations", "users"
 end
