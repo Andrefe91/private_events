946f91a64189c3b9c06b7fd477153df0ac001a47
Joshua <andrefe91@gmail.com>
2024-02-13 16:02:15 -0500

    SQL querys for previous and future events

--- private_events4/app/models/user.rb
+++ private_events4/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   has_many :events
 
-  has_many :invitations #It doesnt require an foreign_key option as the Invitation model already has a User_id column
+  has_many :invitations #It doesnt require a foreign_key option as the Invitation model already has a User_id column
   has_many :attended_events, through: :invitations, source: :event, dependent: :destroy
 
 end
